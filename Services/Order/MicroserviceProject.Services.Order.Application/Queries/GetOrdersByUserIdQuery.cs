6a43bb37c27bbec456f076f468d6eb07fb7b9d1e
fatihsalli <mimsallifatih@gmail.com>
2023-07-11 15:11:44 +0300

    MediatR

---
+++ Services/Order/MicroserviceProject.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using MicroserviceProject.Services.Order.Application.Dtos;
+using MicroserviceProject.Shared.Responses;
+
+namespace MicroserviceProject.Services.Order.Application.Queries;
+
+// IRequest ile MediatR kullanarak işaretledik CustomResponse ile de response u belirtiyoruz.
+public class GetOrdersByUserIdQuery : IRequest<CustomResponse<List<OrderDto>>>
+{
+    public string UserId { get; set; }
+}
