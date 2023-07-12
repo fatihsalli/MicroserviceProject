ae376aaa7e3a74094704037ea4ca082d95565b6d
fatihsalli <mimsallifatih@gmail.com>
2023-07-12 10:15:06 +0300

    CQRS

---
+++ Services/Order/MicroserviceProject.Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using MicroserviceProject.Services.Order.Application.Dtos;
+using MicroserviceProject.Services.Order.Domain.OrderAggregate;
+using MicroserviceProject.Shared.Responses;
+
+namespace MicroserviceProject.Services.Order.Application.Commands;
+
+public class CreateOrderCommand:IRequest<CustomResponse<CreatedOrderDto>>
+{
+    public string UserId { get; set; }
+    public List<OrderItemDto> OrderItems { get; set; }
+    public AddressDto Address { get; set; }
+}
