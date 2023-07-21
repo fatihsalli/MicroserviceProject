7102e33dc0fe826fc7e932f478eb58140799c7be
fatihsalli <mimsallifatih@gmail.com>
2023-07-21 14:01:49 +0300

    GetOrdersByUserId

--- Services/Order/MicroserviceProject.Services.Order.API/Controllers/OrdersController.cs
+++ Services/Order/MicroserviceProject.Services.Order.API/Controllers/OrdersController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Net;
 using MediatR;
-using MicroserviceProject.Services.Order.Application.Dtos;
 using MicroserviceProject.Services.Order.Application.Dtos.Requests;
 using MicroserviceProject.Services.Order.Application.Dtos.Responses;
 using MicroserviceProject.Services.Order.Application.Orders.Commands.CreateOrder;
@@ -8,6 +7,7 @@ using MicroserviceProject.Services.Order.Application.Orders.Commands.DeleteOrder
 using MicroserviceProject.Services.Order.Application.Orders.Commands.UpdateOrder;
 using MicroserviceProject.Services.Order.Application.Orders.Queries.GetAllOrders;
 using MicroserviceProject.Services.Order.Application.Orders.Queries.GetOrderById;
+using MicroserviceProject.Services.Order.Application.Orders.Queries.GetOrdersByUserId;
 using MicroserviceProject.Shared.BaseController;
 using MicroserviceProject.Shared.Responses;
 using Microsoft.AspNetCore.Mvc;
@@ -42,6 +42,15 @@ public class OrdersController : CustomBaseController
         return CreateActionResult(response);
     }
 
+    [HttpGet("[action]")]
+    [ProducesResponseType(typeof(CustomResponse<List<OrderResponse>>), (int)HttpStatusCode.OK)]
+    [ProducesResponseType(typeof(CustomResponse<NoContent>), (int)HttpStatusCode.InternalServerError)]
+    public async Task<IActionResult> GetOrdersByUserId([FromQuery] string userId)
+    {
+        var response = await _mediator.Send(new GetOrdersByUserIdQuery { UserId = userId });
+        return CreateActionResult(response);
+    }
+
     [HttpPost]
     [ProducesResponseType(typeof(CustomResponse<CreatedOrderResponse>), (int)HttpStatusCode.Created)]
     [ProducesResponseType(typeof(CustomResponse<NoContent>), (int)HttpStatusCode.BadRequest)]
@@ -62,7 +71,7 @@ public class OrdersController : CustomBaseController
         var response = await _mediator.Send(new UpdateOrderCommand { Id = id, Address = addressRequest });
         return CreateActionResult(response);
     }
-    
+
     [HttpDelete("{id:length(36)}")]
     [ProducesResponseType(typeof(CustomResponse<bool>), (int)HttpStatusCode.OK)]
     [ProducesResponseType(typeof(CustomResponse<NoContent>), (int)HttpStatusCode.NotFound)]
---
+++ Services/Order/MicroserviceProject.Services.Order.Application/Orders/Queries/GetOrdersByUserId/GetOrdersByUserIdQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MicroserviceProject.Shared.Helpers;
+
+namespace MicroserviceProject.Services.Order.Application.Orders.Queries.GetOrdersByUserId;
+
+public class GetOrdersByUserIdQueryValidator : AbstractValidator<GetOrdersByUserIdQuery>
+{
+    public GetOrdersByUserIdQueryValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotNull().WithMessage("{PropertyName} is required")
+            .NotEmpty().WithMessage("{PropertyName} is required")
+            .Must(Helpers.BeValidGuid).WithMessage("{PropertyName} must be uuid");
+    }
+}
+
