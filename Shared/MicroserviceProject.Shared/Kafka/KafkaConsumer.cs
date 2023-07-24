d6baa7c8d13226123a87dba364b5329d54cbdf22
fatihsalli <mimsallifatih@gmail.com>
2023-07-24 15:46:49 +0300

    Kafka

---
+++ Shared/MicroserviceProject.Shared/Kafka/KafkaConsumer.cs
@@ -0,0 +1,92 @@
+﻿using Confluent.Kafka;
+
+namespace MicroserviceProject.Shared.Kafka;
+
+public class KafkaConsumer : IDisposable
+{
+   private IConsumer<string, string> consumer;
+    private List<TopicPartitionOffset> messageOffsets;
+
+    public KafkaConsumer(string kafkaURL, string groupId, int bulkConsumeMaxTimeoutInSeconds)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = kafkaURL,
+            GroupId = groupId,
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            EnableAutoCommit = false // Disable auto commit to manually commit offsets
+        };
+
+        consumer = new ConsumerBuilder<string, string>(config).Build();
+        messageOffsets = new List<TopicPartitionOffset>();
+    }
+
+    public void SubscribeToTopics(IEnumerable<string> topics)
+    {
+        consumer.Subscribe(topics);
+    }
+
+    public List<Message<string, string>> ConsumeFromTopics(int bulkConsumeIntervalInSeconds, int bulkConsumeMaxTimeoutInSeconds, int maxReadCount)
+    {
+        var messages = new List<Message<string, string>>();
+        var timeoutCount = 0;
+        var start = DateTime.Now;
+
+        while (true)
+        {
+            try
+            {
+                var consumeResult = consumer.Consume(new TimeSpan(0, 0, bulkConsumeMaxTimeoutInSeconds));
+
+                if (consumeResult == null)
+                {
+                    timeoutCount++;
+                    if (timeoutCount > 2)
+                    {
+                        break;
+                    }
+                    continue;
+                }
+
+                if (consumeResult.Message != null)
+                {
+                    messages.Add(consumeResult.Message);
+                }
+
+                messageOffsets.Add(consumeResult.TopicPartitionOffset);
+
+                var elapsedTime = DateTime.Now - start;
+                if (elapsedTime.Seconds > bulkConsumeIntervalInSeconds || messages.Count >= maxReadCount)
+                {
+                    break;
+                }
+            }
+            catch (ConsumeException e)
+            {
+                Console.WriteLine($"Kafka read messages failed. | Error: {e.Error.Reason}");
+                if (messages.Count > 0)
+                {
+                    break;
+                }
+            }
+        }
+
+        return messages;
+    }
+
+    public void CommitOffsets()
+    {
+        if (messageOffsets.Count > 0)
+        {
+            consumer.Commit(messageOffsets);
+            messageOffsets.Clear();
+        }
+    }
+
+    public void Dispose()
+    {
+        CommitOffsets(); // Commit remaining offsets before closing
+        consumer.Close();
+        consumer.Dispose();
+    }
+}
---
+++ Shared/MicroserviceProject.Shared/Kafka/KafkaProducer.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+using Serilog;
+
+namespace MicroserviceProject.Shared.Kafka;
+
+public class KafkaProducer
+{
+    private readonly IProducer<string, string> _producer;
+    
+    public KafkaProducer(string kafkaHost)
+    {
+        var config = new ProducerConfig { BootstrapServers = kafkaHost };
+        _producer = new ProducerBuilder<string, string>(config).Build();
+    }
+    
+    public void SendToKafkaWithMessage(string message, string topic)
+    {
+        try
+        {
+            var deliveryReport = _producer.ProduceAsync(topic, new Message<string, string> { Value = message });
+            deliveryReport.ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    if (task.Exception != null) Log.Error("Delivery failed: {ErrorReason}", task.Exception.Message);
+                }
+                else
+                {
+                    Log.Information("Delivered message to: {ResultTopicPartitionOffset}", task.Result.TopicPartitionOffset);
+                }
+            });
+        }
+        catch (ProduceException<string, string> e)
+        {
+            Log.Error("Delivery failed: {ErrorReason}", e.Error.Reason);
+        }
+    }
+
+    public void Dispose()
+    {
+        _producer.Flush(TimeSpan.FromSeconds(10));
+        _producer.Dispose();
+    }
+}
