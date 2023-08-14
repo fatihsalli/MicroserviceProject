30ce2c6b9e488c90138c6780d39f919fa12c3641
fatihsalli <mimsallifatih@gmail.com>
2023-08-14 11:52:02 +0300

    Product

---
+++ Services/Product/MicroserviceProject.Services.Product.API/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using MicroserviceProject.Shared.Models;
+using MicroserviceProject.Shared.Models.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceProject.Services.Product.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        
+
+
+
+    }
+}
